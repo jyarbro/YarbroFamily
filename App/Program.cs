3e7d6bb02cad384b28c66b2eca986d768a26d3c6
Jim Yarbro <nrrdio@outlook.com>
2020-11-11 22:16:33 +0100

    format

--- App/Program.cs
+++ App/Program.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System.Net;
 
-namespace App
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace App {
+    public class Program {
+        public static void Main(string[] args) {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) => 
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
