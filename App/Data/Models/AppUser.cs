e1148264b4387f906b874d7ae68a74f3f541aa27
Jim Yarbro <nrrdio@outlook.com>
2020-11-11 22:17:35 +0100

    format

--- App/Data/Models/AppUser.cs
+++ App/Data/Models/AppUser.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace App.Data.Models {
+﻿namespace App.Data.Models {
     public class AppUser {
         public string Id { get; set; }
     }
