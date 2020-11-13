2b95dd7b7b7e7bf1d96345d9e7b18ed316078b32
Jim Yarbro <nrrdio@outlook.com>
2020-11-13 22:33:45 +0100

    Implemented multiple handlers for create page.

--- App/Areas/Homes/Pages/Home/Create.cshtml.cs
+++ App/Areas/Homes/Pages/Home/Create.cshtml.cs
@@ -14,6 +14,7 @@ namespace App.Areas.Homes.Pages.Home {
         readonly AppUserService AppUsers;
                 
         [BindProperty] public InputModel Input { get; set; }
+        public bool Confirmed { get; set; }
 
         public CreateModel(
             DataContext dataContext,
@@ -25,83 +26,71 @@ namespace App.Areas.Homes.Pages.Home {
 
         public IActionResult OnGet() => Page();
 
-        public async Task<IActionResult> OnPostAsync() {
+        public IActionResult OnPost() {
+            Confirmed = true;
+
+            var parts = Input.Address.Split(",");
+
+            if (parts.Length > 0) {
+                var subParts = parts[0].Trim().Split(" ").ToList();
+
+                if (subParts.Count > 1) {
+                    Input.HouseNumber = subParts[0].Trim();
+                    subParts.RemoveAt(0);
+
+                    Input.StreetName = string.Join(" ", subParts).Trim();
+                }
+            }
+
+            if (parts.Length > 1) {
+                Input.City = parts[1];
+            }
+
+            if (parts.Length > 2) {
+                var subParts = parts[2].Trim().Split(" ");
+
+                if (subParts.Length > 1) {
+                    Input.State = subParts[0].Trim();
+                    Input.Zip = Convert.ToInt32(subParts[1].Trim());
+                }
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostConfirmedAsync() {
             if (!ModelState.IsValid) {
                 return Page();
             }
 
-            if (Input.Confirmed) {
-                var record = await DataContext.Homes.FirstOrDefaultAsync(r => r.Address == Input.Address);
+            var record = await DataContext.Homes.FirstOrDefaultAsync(r => r.Address == Input.Address);
 
-                if (record is not null) {
-                    return RedirectToPage("./Details", new { id = record.Id });
-                }
-
-                var id = await createRecord();
-                return RedirectToPage("./Details", new { id });
-            }
-            else {
-                generateAddressValues();
-                return Page();
+            if (record is not null) {
+                return RedirectToPage("./Details", new { record.Id });
             }
 
-            async Task<int> createRecord() {
-                var appUser = await AppUsers.Get(User);
+            var appUser = await AppUsers.Get(User);
 
-                var record = new Data.Models.Home {
-                    Address = Input.Address,
-                    HouseNumber = Input.HouseNumber,
-                    StreetName = Input.StreetName,
-                    City = Input.City,
-                    State = Input.State,
-                    Zip = Input.Zip,
-                    CreatedById = appUser.Id,
-                    Created = DateTime.Now,
-                    ModifiedById = appUser.Id,
-                    Modified = DateTime.Now
-                };
+            record = new Data.Models.Home {
+                Address = Input.Address,
+                HouseNumber = Input.HouseNumber,
+                StreetName = Input.StreetName,
+                City = Input.City,
+                State = Input.State,
+                Zip = Input.Zip,
+                CreatedById = appUser.Id,
+                Created = DateTime.Now,
+                ModifiedById = appUser.Id,
+                Modified = DateTime.Now
+            };
 
-                DataContext.Homes.Add(record);
-                await DataContext.SaveChangesAsync();
+            DataContext.Homes.Add(record);
+            await DataContext.SaveChangesAsync();
 
-                return record.Id;
-            }
-
-            void generateAddressValues() {
-                var parts = Input.Address.Split(",");
-
-                if (parts.Length > 0) {
-                    var subParts = parts[0].Trim().Split(" ").ToList();
-
-                    if (subParts.Count > 1) {
-                        Input.HouseNumber = subParts[0].Trim();
-                        subParts.RemoveAt(0);
-
-                        Input.StreetName = string.Join(" ", subParts).Trim();
-                    }
-                }
-
-                if (parts.Length > 1) {
-                    Input.City = parts[1];
-                }
-
-                if (parts.Length > 2) {
-                    var subParts = parts[2].Trim().Split(" ");
-
-                    if (subParts.Length > 1) {
-                        Input.State = subParts[0].Trim();
-                        Input.Zip = Convert.ToInt32(subParts[1].Trim());
-                    }
-                }
-
-                Input.Confirmed = true;
-            }
+            return RedirectToPage("./Details", new { record.Id });
         }
 
         public class InputModel {
-            [HiddenInput]
-            public bool Confirmed { get; set; }
-            
             [Required]
             [Display(Name = "Street Address")]
             [MaxLength(256)]
