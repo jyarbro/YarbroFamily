597d826d14be82873084a255621e74f3cf64816e
Jim Yarbro <nrrdio@outlook.com>
2020-11-13 17:02:50 +0100

    added value

--- App/Data/Models/DetailWeight.cs
+++ App/Data/Models/DetailWeight.cs
@@ -5,6 +5,7 @@ namespace App.Data.Models
     public class DetailWeight
     {
         public int Id { get; set; }
+        public int Value { get; set; }
 
         public string UserId { get; set; }
         public AppUser User { get; set; }
