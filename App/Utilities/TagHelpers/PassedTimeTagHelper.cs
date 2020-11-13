fe4b53d3da49473028d6c6005287c5d7c07f2e73
Jim Yarbro <nrrdio@outlook.com>
2020-11-13 16:37:14 +0100

    taghelper

---
+++ App/Utilities/TagHelpers/PassedTimeTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace App.Utilities.TagHelpers {
+    public class PassedTimeTagHelper : TagHelper {
+        public DateTime Time { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            output.TagName = "time";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("datetime", Time.ToHtmlLocalTimeString());
+            output.Attributes.SetAttribute("title", Time);
+
+            if (output.Content.GetContent() is not { Length: > 0 }) {
+                output.Content.SetContent(Time.ToPassedTimeString());
+            }
+        }
+    }
+}
