6179a6fcd0dbc5ec8a438333268c85ea33429664
Jim Yarbro <nrrdio@outlook.com>
2020-11-15 17:09:49 +0100

    Improved layout for index and details

--- App/Areas/Homes/Pages/Home/Index.cshtml.cs
+++ App/Areas/Homes/Pages/Home/Index.cshtml.cs
@@ -1,7 +1,8 @@
 ﻿using App.Data;
 using App.Data.Services;
-using App.Utilities;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,15 @@ namespace App.Areas.Homes.Pages.Home {
         readonly DataContext DataContext;
         readonly AppUserService AppUserService;
         readonly HomeService HomeService;
+
+        [BindProperty] public string Sort { get; set; }
+
+        public IList<SelectListItem> SortOptions => new List<SelectListItem> {
+            new SelectListItem { Text = "Score", Value = "score" },
+            new SelectListItem { Text = "Address", Value = "address" },
+            new SelectListItem { Text = "Last Updated", Value = "updated" },
+            new SelectListItem { Text = "Created Date", Value = "created" },
+        };
 
         public IndexModel(
             DataContext dataContext,
@@ -27,11 +37,11 @@ namespace App.Areas.Homes.Pages.Home {
         public IList<HomeModel> Homes { get; set; }
 
         public async Task OnGetAsync(string sort) {
+            Sort = sort;
+            
             await AppUserService.Get(User);
 
             Homes = new List<HomeModel>();
-
-            ViewData["sort"] = sort ?? "score";
 
             var users = await DataContext.AppUsers.ToListAsync();
 
@@ -39,21 +49,18 @@ namespace App.Areas.Homes.Pages.Home {
                 .Include(h => h.CreatedBy)
                 .Include(h => h.ModifiedBy).ToListAsync();
 
-            if (sort is "address") {
-                homeRecords = homeRecords
-                    .OrderBy(o => o.City)
-                    .ThenBy(o => o.StreetName)
-                    .ThenBy(o => o.HouseNumber)
-                    .ToList();
-            }
-
             foreach (var home in homeRecords) {
                 var homeModel = new HomeModel {
                     Id = home.Id,
                     Address = home.Address,
+                    HouseNumber = home.HouseNumber,
+                    StreetName = home.StreetName,
+                    City = home.City,
                     Score = HomeService.HomeScore(home),
                     Updated = home.Modified,
                     Created = home.Created,
+                    UpdatedBy = home.ModifiedBy.FirstName,
+                    CreatedBy = home.CreatedBy.FirstName,
                     UserScores = new List<UserScoreModel>()
                 };
 
@@ -67,26 +74,42 @@ namespace App.Areas.Homes.Pages.Home {
                 }
             }
 
-            if (sort is null or "score") {
-                Homes = Homes.OrderByDescending(o => o.Score).ToList();
-            }
+            switch (Sort) {
+                default:
+                case "score":
+                    Homes = Homes.OrderByDescending(o => o.Score).ToList();
+                    break;
 
-            if (sort is "updated") {
-                Homes = Homes.OrderByDescending(o => o.Updated).ToList();
-            }
+                case "updated":
+                    Homes = Homes.OrderByDescending(o => o.Updated).ToList();
+                    break;
 
-            if (sort is "created") {
-                Homes = Homes.OrderByDescending(o => o.Created).ToList();
+                case "created":
+                    Homes = Homes.OrderByDescending(o => o.Created).ToList();
+                    break;
+
+                case "address":
+                    Homes = Homes
+                        .OrderBy(o => o.City)
+                        .ThenBy(o => o.StreetName)
+                        .ThenBy(o => o.HouseNumber)
+                        .ToList();
+                    break;
             }
         }
 
         public class HomeModel {
             public int Id { get; set; }
             public string Address { get; set; }
+            public string HouseNumber { get; set; }
+            public string StreetName { get; set; }
+            public string City { get; set; }
             public int Score { get; set; }
             public IList<UserScoreModel> UserScores { get; set; }
             public DateTime Updated { get; set; }
             public DateTime Created { get; set; }
+            public string UpdatedBy { get; set; }
+            public string CreatedBy { get; set; }
         }
 
         public class UserScoreModel {
